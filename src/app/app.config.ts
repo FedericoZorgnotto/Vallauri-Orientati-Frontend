ff10acbdc71f6b804376179a6f94319b1d4fe46b
Gaga Lavoro <gferrero@siapcn.it>
2025-10-07 10:42:54 +0200

    add: Creato Nuovo progetto angular V20

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,31 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { tokenInterceptor } from './core/interceptors/token/token.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideHttpClient(withInterceptors([tokenInterceptor])),
+    provideRouter(routes)
   ]
 };
-
-
-import { InjectionToken } from '@angular/core';
-
-export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL', {
-  factory: () => {
-    const ps = "8000";
-    const pl = "8000";
-    const ep = "api/v1/";
-
-    if (location.origin === 'file://') {
-      return `https://localhost:${pl}/${ep}`;
-    } else {
-      return `https://${location.hostname}:${ps}/${ep}`;
-    }
-  }
-});
